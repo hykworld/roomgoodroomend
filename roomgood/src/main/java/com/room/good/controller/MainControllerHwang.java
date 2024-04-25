69fc336d5aebd640ad29910f36299ef42d919072
kkm <kummo9@naver.com>
2024-04-25 10:21:13 +0900

    4월 25일 오전 10 시 20분 비밀번호변경 페이지 및 회원정보 마무리 완료 .구건모의 푸쉬

--- roomgood/src/main/java/com/room/good/controller/MainControllerHwang.java
+++ roomgood/src/main/java/com/room/good/controller/MainControllerHwang.java
@@ -3,9 +3,15 @@ package com.room.good.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @Log4j2
 @RequiredArgsConstructor
 public class MainControllerHwang {
+
+    @GetMapping("/productregister")
+    public void productregisterget(){}
+    
+
 }
--- roomgood/src/main/main/java/com/room/good/entity/ClubMemberRole.java
+++
@@ -1,6 +0,0 @@
-package com.room.good.entity;
-
-public enum ClubMemberRole {
-
-    USER,MANAGER,ADMIN,VIP,VVIP
-}
