29519b28e355f876219e62345c7b8dc8c3532ebb
psm <pkms5@naver.com>
2024-05-13 18:02:43 +0900

    24 05 13 메인페이지 수정 중 2번째

--- roomgood/src/main/java/com/room/good/entity/TimeSale.java
+++ roomgood/src/main/java/com/room/good/entity/TimeSale.java
@@ -29,6 +29,12 @@ public class TimeSale {
     //상품페이지 어디로 이동될껀지 url
     private String url;
 
+    //오리지널 가격
+    private Long originalPrice;
+
+    //할인율
+    private Long sale;
+
     //끝나는 시간 년 월 일 시간순으로 입력해야하고 이건 일단 하드코딩할 예정
     private String endTime;
 }
